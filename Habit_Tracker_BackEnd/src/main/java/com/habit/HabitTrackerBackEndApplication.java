343723fbdf33d31b26fad12aea6f0f91549c729c
KakarlaBharath9 <kakarlabharath9@gmail.com>
2025-11-11 14:58:34 +0530

    added all folders and files to git-repo

---
+++ Habit_Tracker_BackEnd/src/main/java/com/habit/HabitTrackerBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.habit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HabitTrackerBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HabitTrackerBackEndApplication.class, args);
+	}
+
+}
