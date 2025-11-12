47d7bba94904bcccb015945dc48a274e72aabc2d
KakarlaBharath9 <kakarlabharath9@gmail.com>
2025-11-12 23:05:33 +0530

    updated files

--- Habit_Tracker_BackEnd/src/main/java/com/habit/service/UserDetailsServiceImpl.java
+++ Habit_Tracker_BackEnd/src/main/java/com/habit/service/UserDetailsServiceImpl.java
@@ -1,5 +1,28 @@
 package com.habit.service;
 
-public class UserDetailsServiceImpl {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.User.UserBuilder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
 
+import com.habit.entities.User;
+import com.habit.repositories.UserRepository;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+	@Autowired
+	private UserRepository userRepository;
+	
+	@Override
+	public UserDetails loadUserByUsername(String username)throws UsernameNotFoundException{
+		User user = userRepository.findByUsername(username)
+				.orElseThrow(()->new UsernameNotFoundException("User Not Found with username: "+username));
+		UserBuilder builder = org.springframework.security.core.userdetails.User.withUsername(user.getUsername());
+		builder.password(user.getPassword());
+		builder.roles("User");
+		return builder.build();
+		
+	}
 }
