ccc2841d90d39090f437dbaf7308027adc07c361
KakarlaBharath9 <kakarlabharath9@gmail.com>
2025-11-20 14:50:15 +0530

    updated files

--- habit-tracker-frontend/src/App.js
+++ habit-tracker-frontend/src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router,Routes,Route } from "react-router-dom";
+import Header from"./components/Header";
+import LoginPage from "./pages/LoginPage";
+import RegisterPage from "./pages/RegisterPage";
+import Dashboard from "./pages/Dashboard";
+import HabitForm from "./pages/HabitForm";
+import HabitDetails from "./pages/HabitDetails";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+function App(){
+  return(
+    <Router>
+      <Header/>
+      <Routes>
+        <Route path="/" element={<Dashboard/>} />
+        <Route path="/login" element={<LoginPage/>} />
+        <Route path="/register" element={<RegisterPage/>} />
+        <Route path="/habit/new" element={<HabitForm />} />
+        <Route path="/habit/:id" element={<HabitDetails/>} />
+      </Routes>
+    </Router>
   );
-}
-
-export default App;
+}export default App;
